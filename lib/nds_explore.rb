88a03ad545d4a0642df56da2a20286167d23765e
Antonio Blandin <antonio.blandin25@gmail.com>
2019-12-21 07:40:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,6 +11,13 @@ end
 
 def print_first_directors_movie_titles
   
-  
+  def print_first_directors_movie_titles
+  i = 0
+  movies = directors_database[0][:movies]
+  while i < movies.count do
+    puts movies[i][:title]
+    i += 1
+  end
+end
   
 end
